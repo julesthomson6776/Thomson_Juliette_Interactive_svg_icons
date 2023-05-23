b763f24952ce8bd1ea72785753dd2ed135259f5f
julesthomson6776 <121986353+julesthomson6776@users.noreply.github.com>
2023-05-23 16:21:46 -0400

    adding the fruit and adding javascript

---
+++ js/main.js
@@ -0,0 +1,53 @@
+console.log("JS file connected")
+
+const grapes = document.querySelector("#grapes");
+
+function logId() {
+    console.log (this.id);
+}
+
+grapes.addEventListener("click", logId)
+
+
+const mango = document.querySelector("#mango");
+
+function logId() {
+    console.log (this.id);
+}
+
+mango.addEventListener("click", logId)
+
+const orange = document.querySelector("#orange");
+
+function logId() {
+    console.log (this.id);
+}
+
+orange.addEventListener("click", logId)
+
+const pear = document.querySelector("#pear");
+
+function logId() {
+    console.log (this.id);
+}
+
+pear.addEventListener("click", logId)
+
+
+const strawberry = document.querySelector("#strawberry");
+
+function logId() {
+    console.log (this.id);
+}
+
+strawberry.addEventListener("click", logId)
+
+const watermellon = document.querySelector("#watermellon");
+
+function logId() {
+    console.log (this.id);
+}
+
+watermellon.addEventListener("click", logId)
+
+
